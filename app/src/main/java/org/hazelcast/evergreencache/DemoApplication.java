a92a43b2f90660258312c0e0b84e8d7374cc37ae
Nicolas Frankel <nicolas@frankel.ch>
2020-05-05 10:44:36 +0200

    Add caching capabilities

--- app/src/main/java/org/hazelcast/evergreencache/DemoApplication.java
+++ app/src/main/java/org/hazelcast/evergreencache/DemoApplication.java
@@ -1,7 +1,10 @@
 package org.hazelcast.evergreencache;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -9,8 +12,15 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class DemoApplication {
 
     @Bean
-    public PersonRepository repository(JdbcTemplate template) {
-        return new PersonRepository(template);
+    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
+        return new HazelcastCacheManager(hazelcastInstance);
+    }
+
+    @Bean
+    public PersonRepository repository(JdbcTemplate template, CacheManager cacheManager) {
+        return new PersonRepository(template,
+                cacheManager.getCache("entities"),
+                cacheManager.getCache("query"));
     }
 
     public static void main(String[] args) {
