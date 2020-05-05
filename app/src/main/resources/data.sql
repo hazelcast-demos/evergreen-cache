078600a57745eaad25790da6c71d8fd98a052823
Nicolas Frankel <nicolas@frankel.ch>
2020-05-05 07:19:33 +0200

    Initial commit

---
+++ app/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Person(id, firstName, lastName, birthdate) VALUES (1, 'John', 'Doe', '1971-02-03');
+INSERT INTO Person(id, firstName, lastName, birthdate) VALUES (2, 'Jane', 'Doe', '1974-05-06');
+INSERT INTO Person(id, firstName, lastName)            VALUES (3, 'John', 'Roe');
