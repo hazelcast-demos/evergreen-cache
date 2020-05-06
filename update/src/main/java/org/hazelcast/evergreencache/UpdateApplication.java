8fe53fac95f39176ee6c7fd6dff9112eb61898e3
Nicolas Frankel <nicolas@frankel.ch>
2020-05-06 16:53:13 +0200

    Provide shell commands to sneakily update data in the database

---
+++ update/src/main/java/org/hazelcast/evergreencache/UpdateApplication.java
@@ -0,0 +1,13 @@
+package org.hazelcast.evergreencache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpdateApplication.class, args);
+    }
+
+}
